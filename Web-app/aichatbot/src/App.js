087791fad78efdd1e4917d6367bdf69fe239d0ba
DEEPAKINDRESH N <deepakindreshn@gmail.com>
2022-03-29 21:40:47 +0530

    All files uploaded

--- Web-app/aichatbot/src/App.js
+++ Web-app/aichatbot/src/App.js
@@ -1,26 +1,30 @@
 import React, { useRef, useState } from 'react';
 import './App.css';
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
-import { getFirestore, collection, getDocs, addDoc, query, orderBy, limit  } from "firebase/firestore"
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
+
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
-const firebaseConfig = {
-        apiKey: process.env.REACT_APP_API_KEY,
-        authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-        projectId: process.env.REACT_APP_PROJECT_ID,
-        storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-        messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-        appId: process.env.REACT_APP_ID,
-        measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-};
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const firestore = getFirestore(app);
-const analytics = getAnalytics(app);
+import env from './env.js';
+
+
+firebase.initializeApp({
+  apiKey: env.REACT_APP_API_KEY,
+  authDomain: env.REACT_APP_AUTH_DOMAIN,
+  projectId: env.REACT_APP_PROJECT_ID,
+  storageBucket: env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: env.REACT_APP_ID,
+  measurementId: env.REACT_APP_MEASUREMENT_ID,
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
 
 
 function App() {
@@ -30,7 +34,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>⚛️🔥💬</h1>
+        <h1>💬🤖</h1>
         <SignOut />
       </header>
 
@@ -45,8 +49,8 @@ function App() {
 function SignIn() {
 
   const signInWithGoogle = () => {
-    const provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider)
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
   }
 
   return (
@@ -60,16 +64,17 @@ function SignIn() {
 
 function SignOut() {
   return auth.currentUser && (
-    <button className="sign-out" onClick={() => signOut(auth)}>Sign Out</button>
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
   )
 }
 
 
-async function ChatRoom() {
+function ChatRoom() {
   const dummy = useRef();
-  const messagesRef = await getDocs(collection(firestore, "messages"));
-  const q = query(messagesRef, orderBy("createdAt"), limit(25));
-  const [messages] = useCollectionData(q, { idField: 'id' });
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, { idField: 'id' });
 
   const [formValue, setFormValue] = useState('');
 
@@ -81,7 +86,7 @@ async function ChatRoom() {
 
     await messagesRef.add({
       text: formValue,
-      createdAt: firestore.FieldValue.serverTimestamp(),
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
       uid,
       photoURL
     })
---
+++ Web-app/aichatbot/src/env.js
@@ -0,0 +1,9 @@
+export default {
+    REACT_APP_API_KEY: "AIzaSyDSOBLJIGALl2AlHvFlwMII849UNxDfSf4",
+    REACT_APP_AUTH_DOMAIN:  "ai-chatbot-cf354.firebaseapp.com",
+    REACT_APP_PROJECT_ID: "ai-chatbot-cf354",
+    REACT_APP_STORAGE_BUCKET: "ai-chatbot-cf354.appspot.com",
+    REACT_APP_MESSAGING_SENDER_ID: "965815603509",
+    REACT_APP_ID: "1:965815603509:web:35203d2bc462c9eb003c7c",
+    REACT_APP_MEASUREMENT_ID: "G-S2CE3W7F9Q",
+}
