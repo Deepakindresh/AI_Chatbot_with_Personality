3a8fdc1421a4be7b915c72574a02e9228c2a1d3e
DEEPAKINDRESH N <deepakindreshn@gmail.com>
2022-04-27 10:44:04 +0530

    Working model

---
+++ Model/tarpbot.py
@@ -0,0 +1,114 @@
+from keras import models
+import re
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Embedding, LSTM, Input
+from tensorflow.keras.layers import Input
+import numpy as np
+from keras.preprocessing.sequence import pad_sequences
+import pickle
+
+
+def clean_text(txt):
+    txt = txt.lower()
+    txt = re.sub(r"i'm", "i am", txt)
+    txt = re.sub(r"he's", "he is", txt)
+    txt = re.sub(r"she's", "she is", txt)
+    txt = re.sub(r"that's", "that is", txt)
+    txt = re.sub(r"what's", "what is", txt)
+    txt = re.sub(r"where's", "where is", txt)
+    txt = re.sub(r"\'ll", " will", txt)
+    txt = re.sub(r"\'ve", " have", txt)
+    txt = re.sub(r"\'re", " are", txt)
+    txt = re.sub(r"\'d", " would", txt)
+    txt = re.sub(r"won't", "will not", txt)
+    txt = re.sub(r"can't", "can not", txt)
+    txt = re.sub(r"[^\w\s]", "", txt)
+    return txt
+
+
+dic1_file = open('vocab', 'rb')
+dic2_file = open('inv_vocab', 'rb')
+vocab = pickle.load(dic1_file)
+inv_vocab = pickle.load(dic2_file)
+
+
+print("##########################################")
+print("#       start chatting ver. 1.0          #")
+print("##########################################")
+
+
+enc_model = models.load_model('enc_model.h5')
+dec_model = models.load_model('dec_model.h5')
+
+prepro1 = ""
+VOCAB_SIZE = len(vocab)
+dense = Dense(VOCAB_SIZE, activation='softmax')
+while prepro1 != 'q':
+    prepro1 = input("you : ")
+    ## prepro1 = "Hello"
+
+    prepro1 = clean_text(prepro1)
+    ## prepro1 = "hello"
+
+    prepro = [prepro1]
+    ## prepro1 = ["hello"]
+
+    txt = []
+    for x in prepro:
+        # x = "hello"
+        lst = []
+        for y in x.split():
+            ## y = "hello"
+            try:
+                lst.append(vocab[y])
+                ## vocab['hello'] = 454
+            except:
+                lst.append(vocab['<OUT>'])
+        txt.append(lst)
+
+    ## txt = [[454]]
+    txt = pad_sequences(txt, 30, padding='post')
+
+    # txt = [[454,0,0,0,.........30]]
+
+    stat = enc_model.predict(txt)
+
+    empty_target_seq = np.zeros((1, 1))
+    ##   empty_target_seq = [0]
+
+    empty_target_seq[0, 0] = vocab['<SOS>']
+    ##    empty_target_seq = [255]
+
+    stop_condition = False
+    decoded_translation = ''
+
+    while not stop_condition:
+
+        dec_outputs, h, c = dec_model.predict([empty_target_seq] + stat)
+        decoder_concat_input = dense(dec_outputs)
+        # decoder_concat_input = [0.1, 0.2, .4, .0, ...............]
+
+        sampled_word_index = np.argmax(decoder_concat_input[0, -1, :])
+        ## sampled_word_index = [2]
+
+        sampled_word = inv_vocab[sampled_word_index] + ' '
+
+        ## inv_vocab[2] = 'hi'
+        ## sampled_word = 'hi '
+
+        if sampled_word != '<EOS> ':
+            decoded_translation += sampled_word
+
+        if sampled_word == '<EOS> ' or len(decoded_translation.split()) > 30:
+            stop_condition = True
+
+        empty_target_seq = np.zeros((1, 1))
+        empty_target_seq[0, 0] = sampled_word_index
+        # <SOS> - > hi
+        # hi --> <EOS>
+        stat = [h, c]
+
+    print("chatbot attention : ", decoded_translation)
+    print("==============================================")
